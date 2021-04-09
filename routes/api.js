ae073bdd30ad855c37ed3ab662a135a14e7c4dea
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-04-09 13:25:53 -0500

    modified get/put/post routes

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,39 @@
-const db = require("../models")
-module.exports = (app) => {
+const db = require("../models");
 
-    //Get all workouts
+module.exports = function(app) {
+    // App.get to pull up info for the workouts page
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}, (err, workouts) => {
-            if(err){
-                console.log(err);
-            } else {
-                res.json(workouts)
-            }
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    // App.get to pull up info for the range page
+    app.get("/api/workouts/range", ({}, res) => {
+      db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    });
+    // App.post to submit new completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      // App.put to update workouts by MongoDB _id value and update the exercsise body
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
         });
     });
-    
-    //add exercise
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
-        db.Workout.findOneAndUpdate({ _id: params.id},
-            {$push: {exercises:body }},
-            { upsert: true, useFindandModify:false},
-            updatedWorkout => {
-                res.json(updatedWorkout);
-            })
-    });
-
-    //new workout
-    app.post("/api/workouts", (req, res) => {
-        db.Workout.create({})
-            .then(newWorkout => {
-                res.json(newWorkout);
-            });
-    });
-
-}
+};
