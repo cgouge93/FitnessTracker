77263826e887efdf9fa5ff5aad4e255775fd3c75
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-04-12 11:24:14 -0500

    re-worked api routes to use aggregation

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,22 @@ const db = require("../models");
 module.exports = function(app) {
     // App.get to pull up info for the workouts page
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
+        db.Workout.aggregate([
+          {
+            $sort: {
+              day: -1,
+            },
+          },
+        { $limit: 7 },
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: '$exercises.duration'
+          }
+        }
+      }
+    ])
+        .then(dbWorkout => {
             res.json(dbWorkout);
         })
         .catch(err => {
@@ -12,10 +27,27 @@ module.exports = function(app) {
     })
     // App.get to pull up info for the range page
     app.get("/api/workouts/range", ({}, res) => {
-      db.Workout.find({}).then((dbWorkout) => {
-        res.json(dbWorkout);
-      }).catch(err => {
-        res.status(400).json(err);
+      db.Workout.aggregate([
+        {
+          $sort: {
+            day: -1,
+          },
+        },
+        { $limit: 7 },
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: '$exercises.duration'
+            }
+          }
+        }
+      ])
+          .then((dbWorkout) => {
+            dbWorkout.reverse();
+          res.json(dbWorkout);
+      })
+          .catch(err => {
+          res.status(400).json(err);
       });
     });
     // App.post to submit new completed workouts
@@ -28,12 +60,17 @@ module.exports = function(app) {
       });
       // App.put to update workouts by MongoDB _id value and update the exercsise body
       app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-          res.status(400).json(err);
-        });
-    });
+        db.Workout.findByIdAndUpdate(req.params.id, 
+          {
+            $push: {
+              exercises: req.body
+            }
+          })
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      });
 };
