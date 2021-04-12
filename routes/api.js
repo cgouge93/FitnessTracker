d53b21161bd1c9c5a693bf74e333efb1c993f27d
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-04-12 12:12:44 -0500

    corrected stats.js to correctly display day labels on charts

--- routes/api.js
+++ routes/api.js
@@ -1,76 +1,82 @@
 const db = require("../models");
 
-module.exports = function(app) {
-    // App.get to pull up info for the workouts page
+module.exports = (app) => {
     app.get("/api/workouts", (req, res) => {
         db.Workout.aggregate([
-          {
-            $sort: {
-              day: -1,
+            {
+                $sort: {
+                    day: -1,
+                },
             },
-          },
-        { $limit: 7 },
-      {
-        $addFields: {
-          totalDuration: {
-            $sum: '$exercises.duration'
-          }
-        }
-      }
-    ])
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    })
-    // App.get to pull up info for the range page
-    app.get("/api/workouts/range", ({}, res) => {
-      db.Workout.aggregate([
-        {
-          $sort: {
-            day: -1,
-          },
-        },
-        { $limit: 7 },
-        {
-          $addFields: {
-            totalDuration: {
-              $sum: '$exercises.duration'
+            { $limit: 7 },
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: '$exercises.duration'
+                    }
+                }
             }
-          }
-        }
-      ])
-          .then((dbWorkout) => {
-            dbWorkout.reverse();
-          res.json(dbWorkout);
-      })
-          .catch(err => {
-          res.status(400).json(err);
-      });
+
+        ])
+            .then(dbWorkout => {
+                dbWorkout.reverse();
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
-    // App.post to submit new completed workouts
-    app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-          });
-      });
-      // App.put to update workouts by MongoDB _id value and update the exercsise body
-      app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(req.params.id, 
-          {
-            $push: {
-              exercises: req.body
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id,
+            {
+                $push: {
+                    exercises: req.body
+                }
+            })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        console.log(req.body);
+        db.Workout.create(req.body)
+            .then(dbWorkout => {
+                console.log(dbWorkout);
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.aggregate([
+            {
+                $sort: {
+                    day: -1,
+                },
+            },
+            { $limit: 7 },
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: '$exercises.duration'
+                    }
+                }
             }
-          })
-          .then(dbWorkout => {
-            res.json(dbWorkout);
-          })
-          .catch(err => {
-            res.json(err);
-          });
-      });
+        ])
+            .then(dbWorkout => {
+                dbWorkout.reverse();
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
 };
